a21d8a95ac472765f89b8451c328459600817ba8
jdlms <lamorey.joshua@gmail.com>
2025-03-16 16:37:09 +0100

    ctx added to sql db calls

--- sql/database/orders.go
+++ sql/database/orders.go
@@ -1,6 +1,9 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+)
 
 type OrderRepository struct {
 	Db *sql.DB
@@ -12,19 +15,57 @@ type Order struct {
 	Amount  int
 }
 
-func (r *OrderRepository) CreateTable() error {
-	_, err := r.Db.Exec(`CREATE TABLE IF NOT EXISTS orders (
+func (r *OrderRepository) CreateTable(ctx context.Context) error {
+	_, err := r.Db.ExecContext(ctx, `CREATE TABLE IF NOT EXISTS orders (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 		product TEXT,
 		amount INTEGER
 		)`)
 
 	return err
-
 }
 
-func (r *OrderRepository) Insert(order Order) error {
-	_, err := r.Db.Exec("INSERT INTO orders (product, amount) VALUES (?, ?)", order.Product, order.Amount)
+func (r *OrderRepository) Insert(ctx context.Context, order Order) error {
+	_, err := r.Db.ExecContext(ctx, "INSERT INTO orders (product, amount) VALUES (?, ?)", order.Product, order.Amount)
 
+	return err
+}
+
+func (r *OrderRepository) GetAll(ctx context.Context) ([]Order, error) {
+	rows, err := r.Db.QueryContext(ctx, "SELECT * FROM orders")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var orders []Order
+	for rows.Next() {
+		var order Order
+		err := rows.Scan(&order.Id, &order.Product, &order.Amount)
+		if err != nil {
+			return nil, err
+		}
+		orders = append(orders, order)
+	}
+
+	return orders, nil
+}
+
+func (r *OrderRepository) GetById(ctx context.Context, id int) (Order, error) {
+	var order Order
+	err := r.Db.QueryRowContext(ctx, "SELECT * FROM orders WHERE id = ?", id).Scan(&order.Id, &order.Product, &order.Amount)
+	if err != nil {
+		return Order{}, err
+	}
+	return order, nil
+}
+
+func (r *OrderRepository) Update(ctx context.Context, order Order) error {
+	_, err := r.Db.ExecContext(ctx, "UPDATE orders SET product = ?, amount = ? WHERE id = ?", order.Product, order.Amount, order.Id)
+	return err
+}
+
+func (r *OrderRepository) Delete(ctx context.Context, id int) error {
+	_, err := r.Db.ExecContext(ctx, "DELETE FROM orders WHERE id = ?", id)
 	return err
 }
--- sql/main.go
+++ sql/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
+	"time"
 
 	"github.com/jdlms/go-dojo/sql/database"
 	_ "github.com/mattn/go-sqlite3"
@@ -15,10 +17,61 @@ func main() {
 	}
 	defer db.Close()
 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	orderRepository := &database.OrderRepository{Db: db}
 
-	err = orderRepository.CreateTable()
+	err = orderRepository.CreateTable(ctx)
 	if err != nil {
 		log.Fatal("Error creating orders table", err)
 	}
+
+	err = orderRepository.Insert(ctx, database.Order{Product: "Laptop", Amount: 10})
+	if err != nil {
+		log.Fatal("Error inserting order:", err)
+	}
+
+	err = orderRepository.Insert(ctx, database.Order{Product: "Keyboard", Amount: 50})
+	if err != nil {
+		log.Fatal("Error inserting order:", err)
+	}
+
+	orders, err := orderRepository.GetAll(ctx)
+	if err != nil {
+		log.Fatal("Error getting orders:", err)
+	}
+
+	log.Println(orders)
+
+	order, err := orderRepository.GetById(ctx, orders[0].Id)
+	if err != nil {
+		log.Fatal("Error getting order:", err)
+	}
+
+	order.Amount = 1500
+	err = orderRepository.Update(ctx, order)
+	if err != nil {
+		log.Fatal("Error updating order:", err)
+	}
+
+	orders, err = orderRepository.GetAll(ctx)
+	if err != nil {
+		log.Fatal("Error getting orders:", err)
+	}
+
+	log.Println(orders)
+
+	err = orderRepository.Delete(ctx, order.Id)
+	if err != nil {
+		log.Fatal("Error deleting order:", err)
+	}
+
+	orders, err = orderRepository.GetAll(ctx)
+	if err != nil {
+		log.Fatal("Error getting orders:", err)
+	}
+
+	log.Println(orders)
+
 }
